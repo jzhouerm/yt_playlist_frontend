e2d848e0ad3cd64a835cd9d9ae5c289d275b31f0
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2020-11-09 23:46:15 -0500

    11-9-20 edited nav bar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -17,46 +17,52 @@ class Navbar extends React.Component {
 
     render() {
       return (
-        <div>
-          <NavLink
-            to="/my_library"
-            /* set exact so it knows to only set activeStyle when route is deeply equal to link */
-            exact
-            /* add styling to Navlink */
-            style={link}
-            /* add prop for activeStyle */
-            activeStyle={{
-              background: 'gray'
-            }}
-          >My Library</NavLink>
+        <>
+          <br/>
+          <br/>
+          <div>
+            <NavLink
+              to="/my_library"
+              /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+              exact
+              /* add styling to Navlink */
+              style={link}
+              /* add prop for activeStyle */
+              activeStyle={{
+                background: 'gray'
+              }}
+            >My Library</NavLink>
 
-          <NavLink
-            to="/playlists"
-            exact
-            style={link}
-            activeStyle={{
-              background: 'gray'
-            }}
-          >My Playlists</NavLink>
+            <NavLink
+              to="/playlists"
+              exact
+              style={link}
+              activeStyle={{
+                background: 'gray'
+              }}
+            >My Playlists</NavLink>
 
-          <NavLink
-            to="/add_new_video"
-            exact
-            style={link}
-            activeStyle={{
-              background: 'gray'
-            }}
-          >Add New Video</NavLink>
+            <NavLink
+              to="/add_new_video"
+              exact
+              style={link}
+              activeStyle={{
+                background: 'gray'
+              }}
+            >Add New Video</NavLink>
 
-          <NavLink
-            to="/add_new_playlist"
-            exact
-            style={link}
-            activeStyle={{
-              background: 'gray'
-            }}
-          >Add New Playlist</NavLink>
-        </div>
+            <NavLink
+              to="/add_new_playlist"
+              exact
+              style={link}
+              activeStyle={{
+                background: 'gray'
+              }}
+            >Add New Playlist</NavLink>
+          </div>
+          <br/>
+          <br/>
+        </>
       )
     }
   }
